11189b0f41ee87a88721e2623428bc45738f367b
Rui Carmo <rui.carmo@gmail.com>
2026-05-16 22:38:40 +0100

    Test sharded Qwen MTP metadata names

--- cmd/qwenmtpmeta/main_test.go
+++ cmd/qwenmtpmeta/main_test.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"encoding/binary"
 	"encoding/json"
+	"os"
+	"path/filepath"
 	"testing"
 
 	loaderconfig "github.com/rcarmo/go-pherence/loader/config"
 )
+
+func TestSafetensorNamesReadsShardedIndex(t *testing.T) {
+	dir := t.TempDir()
+	header := `{"mtp.fc.weight":{"dtype":"F32","shape":[1],"data_offsets":[0,4]}}`
+	var lenBuf [8]byte
+	binary.LittleEndian.PutUint64(lenBuf[:], uint64(len(header)))
+	shard := append(append([]byte{}, lenBuf[:]...), []byte(header)...)
+	shard = append(shard, 0, 0, 0, 0)
+	if err := os.WriteFile(filepath.Join(dir, "model-00001-of-00001.safetensors"), shard, 0644); err != nil {
+		t.Fatalf("write shard: %v", err)
+	}
+	index := `{"weight_map":{"mtp.fc.weight":"model-00001-of-00001.safetensors"}}`
+	if err := os.WriteFile(filepath.Join(dir, "model.safetensors.index.json"), []byte(index), 0644); err != nil {
+		t.Fatalf("write index: %v", err)
+	}
+	names, err := safetensorNames(dir)
+	if err != nil {
+		t.Fatalf("safetensorNames: %v", err)
+	}
+	if len(names) != 1 || names[0] != "mtp.fc.weight" {
+		t.Fatalf("names=%v", names)
+	}
+}
 
 func TestShouldFailStrict(t *testing.T) {
 	meta := loaderconfig.QwenNativeMTPMetadata{HasNativeMTP: true}
